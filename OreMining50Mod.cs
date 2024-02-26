cbe1c6ce54817c2ea479edfc91e336a5374849b0
Evgeniy A. Tsymbalyuk <greendwin@users.noreply.github.com>
2024-02-26 19:35:51 +0300

    Initial setup on `OreMining50Mod`
    * setup solution and docs
    * add `OnMinedOre` patch

---
+++ OreMining50Mod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Assets.Scripts.Objects.Items;
+using HarmonyLib;
+using StationeersMods.Interface;
+
+
+namespace OreMining50Mod
+{
+    [HarmonyPatch(typeof(Tool), nameof(Tool.OnMinedOre))]
+    class ToolOnMinedOrePatch
+    {
+        public static void Prefix(Ore oreMined)
+        {
+            if (oreMined.Quantity < 50)
+            {
+                oreMined.Quantity = 50;
+            }
+        }
+    }
+
+    class OreMining50Mod : ModBehaviour
+    {
+        public override void OnLoaded(ContentHandler contentHandler)
+        {
+            Harmony harmony = new Harmony("OreMining50Mod");
+            harmony.PatchAll();
+
+            Console.WriteLine("[OreMining50Mod] Loaded!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Ore Mining 50 Mod
+
+*This is a StationeersMods Plugin Mod. It requires Bepinex to be installed with the StationeersMods plugin.
+See: https://github.com/jixxed/StationeersMods*
+
+
+## Description
+
+Don't waste your time on mining.
+All minable chunks are 50g.
